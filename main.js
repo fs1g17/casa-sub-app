ad55d0824cbecc0c8c269728e844cd1e8bb4de84
Fedor Selenskiy <47851444+fs1g17@users.noreply.github.com>
2023-02-06 09:47:55 +0000

    Fixes

--- main.js
+++ main.js
@@ -3,10 +3,12 @@ import { configure } from '@dwp/govuk-casa';
 
 import application from './main-app/main.js';
 
-const app1 = application({ mountUrl: '/main' });
+const mountUrl = '/main';
+
+const app1 = application({ mountUrl });
 
 const parent = express();
-parent.use('/main', app1);
+parent.use(mountUrl, app1);
 
 parent.listen(3000, () => {
   console.log('listening on port 3000');
