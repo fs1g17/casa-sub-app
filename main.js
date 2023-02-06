41427cd4a3af0d539a06a82886561c13f5273b16
Fedor Selenskiy <47851444+fs1g17@users.noreply.github.com>
2023-02-06 10:08:49 +0000

    Test works

--- main.js
+++ main.js
@@ -1,15 +1,35 @@
 import express from 'express';
 import { configure } from '@dwp/govuk-casa';
 
-import application from './main-app/main.js';
+function createApp(mountUrl) {
+  const { mount, ancillaryRouter } = configure({
+    views: ['./views/'],
+    session: {
+      name: 'myappsessionid', // session cookie name 
+      secret: 'secret',       // secret used to sign cookie
+      ttl: 3600,              // (seconds)
+      secure: false
+    }
+  });
 
-const mountUrl = '/main';
+  // I know for sure this is the one that gets called
+  ancillaryRouter.use('/start', (req, res, next) => {
+    res.render('pages/start.njk');
+  });
 
-const app1 = application({ mountUrl });
+  // redirect unknown pages to the welcome page
+  ancillaryRouter.use(/^\/$/, (req, res, next) => {
+    res.redirect(302, `${req.baseUrl}/start`);
+  });
+
+  return mount(express());
+}
+
+const app1 = createApp();
+const app2 = createApp();
 
 const parent = express();
-parent.use(mountUrl, app1);
+parent.use('/one/', app1);
+parent.use('/two/', app2);
 
-parent.listen(3000, () => {
-  console.log('listening on port 3000');
-})
+parent.listen(3000);
