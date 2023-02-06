69c4cd7286f8711dc02151f0eca4c84d0b301ac1
Fedor Selenskiy <47851444+fs1g17@users.noreply.github.com>
2023-02-06 11:40:50 +0000

    Redirect localhost:3000 -> localhost:3000/one

--- main.js
+++ main.js
@@ -10,4 +10,8 @@ const parent = express();
 parent.use('/one/', app1);
 parent.use('/two/', app2);
 
+parent.use(/^\/$/, (req, res, next) => {
+  res.redirect(302, `${req.baseUrl}/one`);
+});
+
 parent.listen(3000);
